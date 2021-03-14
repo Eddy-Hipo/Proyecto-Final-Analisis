ca184f1703858745a9181e723de91c291fd5c0ac
BrendaArcentales <brenda.arcentales@epn.edu.ec>
2021-03-14 00:34:42 -0500

    Add files via upload

---
+++ 2_PulsoPoliticoProvincias/Recopilacion y almacenamineto de datos/yaku.py
@@ -0,0 +1,38 @@
+import tweepy
+import json
+import couchdb
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+#Conexion a couchddb
+ckey = "N31wuT1bPEiM2cmx8ccu1orxb"
+csecret = "O4avAsvM6g10JCgRzAnq3mv2QvBqve518tB4ix78dT6u3DAtvh"
+atoken = "360193692-rekUZMqpMZFsnVvXovgqCuuVfwtp3aO4gui2O8zl"
+asecret = "BW1PeKHghRdZ8YUxYbizidV043U3P7ysxXJvZk8i6AWJX"
+class listener(StreamListener):
+    
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        if "pachakutik" in dictTweet["text"].lower() : 
+            try:
+                dictTweet["_id"] = str(dictTweet['id'])
+                doc = db.save(dictTweet)
+                print ("SAVED" + str(doc) +"=>" + str(data))
+            except:
+                print ("Already exists")
+                pass
+            return True
+    
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+streamListen=listener()
+twitterStream = Stream(auth, streamListen)
+server = couchdb.Server('http://admin:brenda@localhost:5984/')  
+try:
+    db = server.create('twitter-proyecto-yaku')
+except:
+    db = server['twitter-proyecto-yaku']
+twitterStream.filter(locations=[-92.21,-5.02,-75.19,1.88])
